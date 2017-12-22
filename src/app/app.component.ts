139c81da36e3c67166adfc9835ce56df1717628e
jani-123 <janina.rqa@gmail.com>
2017-12-22 06:57:37 -0500

    directivas ngstyle, ngclass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,21 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PlatziSquare';
   lugares:any = [
-    { active: true , nombre:'Floreria la Gardemia' },
-    { active: true , nombre: 'Donas La Caserita' },
-    { active: false , nombre: 'Veterinaria happyDog' },
-    { active: false , nombre: 'Floreria la Gardemia' },
-    { active: true , nombre: 'Donas La Caserita' },
-    { active: true , nombre: 'Veterinaria happyDog' },
+    { plan: 'pagado', cercania: 1, distancia: 1, active: true , nombre:'Floreria la Gardemia' },
+    { plan: 'gratuito', cercania: 1, distancia: 1.8, active: true , nombre: 'Donas La Caserita' },
+    { plan: 'pagado', cercania: 2, distancia: 5, active: false , nombre: 'Veterinaria happyDog' },
+    { plan: 'gratuito', cercania: 3, distancia: 10, active: false , nombre: 'Floreria la Gardemia' },
+    { plan: 'gratuito', cercania: 3, distancia: 35, active: true , nombre: 'Donas La Caserita' },
+    { plan: 'pagado', cercania: 3, distancia: 120, active: true , nombre: 'Veterinaria happyDog' },
   ]; // acepta cualquier tipo de objeto
-
+  personas: any = [
+    { nombres: 'juan perez', edad: 12},
+    { nombres: 'maria garcia', edad: 34 },
+    { nombres: 'ana caredenas', edad: 20 },
+    { nombres: 'juana', edad: 21 },
+    { nombres: 'rocio', edad: 16 },
+    { nombres: 'maripaz', edad: 15 },
+  ];
   lat: number = -16.4571485;
   lng: number = -71.5307593;
   constructor() {
