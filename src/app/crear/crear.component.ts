784379ae3affc1b3ab95c9c5d7213dc947d2237b
jani-123 <janina.rqa@gmail.com>
2018-02-09 23:23:22 -0500

    recuperando archivos

--- src/app/crear/crear.component.ts
+++ src/app/crear/crear.component.ts
@@ -9,11 +9,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CrearComponent {
   lugar: any = {};
-  id:any = null;
+  id: any = null;
   constructor(private lugaresService: LugaresService, private route: ActivatedRoute) {
     this.id = this.route.snapshot.params['id'];
     console.log("id", this.id);
-    if(this.id != 'new'){
+    if (this.id != 'new') {
       this.lugaresService.getLugar(this.id)
         .valueChanges().subscribe((lugar) => {
           this.lugar = lugar;
@@ -26,10 +26,10 @@ export class CrearComponent {
       .subscribe((result) => {
         this.lugar.lat = result.json().results[0].geometry.location.lat;
         this.lugar.lng = result.json().results[0].geometry.location.lng;
-        if(this.id != 'new'){
+        if (this.id != 'new') {
           this.lugaresService.editarLugar(this.lugar);
           alert('Negocio Editado con exito');
-        }else{
+        } else {
           this.lugar.id = Date.now();
           this.lugaresService.guardarLugar(this.lugar);
           alert('Negocio Guardado con Exito');
